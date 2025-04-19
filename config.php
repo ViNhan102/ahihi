7ba338fbafc8922d2b94e38566792790c8c6a4f0
ViNhan102 <slydemon06@gmail.com>
2025-04-19 13:23:08 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+return array(
+    'base' => '/php/ss12/',
+    'db' => array(
+        'host'     => 'localhost',
+        'name'     => 'ProductDB',
+        'username' => 'root',
+        'password' => ''
+    )
+);
